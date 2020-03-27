f2de1d442461c3dd883aaae6e479cbf8875ad45b
Bri Y <bri@bris-air.attlocal.net>
2020-03-26 19:45:38 -0700

    style change, clean up js

--- app.js
+++ app.js
@@ -1,80 +1,82 @@
+//Cache DOM elements
 let simonButtons = {
-    1: document.getElementById('red'),
-    2: document.getElementById('orange'),
-    3: document.getElementById('yellow'),
-    4: document.getElementById('green')
-
+  1: document.getElementById('red'),
+  2: document.getElementById('orange'),
+  3: document.getElementById('yellow'),
+  4: document.getElementById('green')
 }
 
 let startButtons = {
-    1: document.getElementById('start'),
-    2: document.getElementById('reset')
+  1: document.getElementById('start'),
+  2: document.getElementById('reset')
 }
 
+let message = document.getElementById('message')
+
+//Establish empty arrays
 let simonPattern = [];
 let userPattern = [];
 
-function randomColor(x,y){
-    return Math.ceil(Math.random() * (4 - 1) + 1);
-}
+//Functions
+const randomColor = (x, y) => Math.ceil(Math.random() * (4 - 1) + 1);
 
-function lightOn (i) {
-    simonButtons[simonPattern[i]].style.opacity = .2;
-    setTimeout(() => {
-        lightOff(i);
-    }, 500)
+const lightOn = (i) => {
+  simonButtons[simonPattern[i]].style.opacity = .2;
+  setTimeout(() => lightOff(i), 500);
 }
 
 function lightOff(i) {
-    simonButtons[simonPattern[i]].style.opacity = 1;
-    setTimeout(() => {
-        i++
-        if (i < simonPattern.length) {
-            lightOn(i)
-        }
-    }, 500)
+  simonButtons[simonPattern[i]].style.opacity = 1;
+  setTimeout(() => {
+    i++
+    if (i < simonPattern.length) {
+      lightOn(i)
+    }
+  }, 500)
 }
 
+//Game logic
 let startRound = function () {
-    let newColor = randomColor();
-    simonPattern.push(newColor);
-    setTimeout(() => {
-        lightOn(0);
-    }, 500)
+  let newColor = randomColor();
+  simonPattern.push(newColor);
+  setTimeout(() => {
+    lightOn(0);
+  }, 500)
 }
 
 function userInput(b) {
-        userPattern.push(b)
-        if (userPattern[userPattern.length - 1] !== simonPattern[userPattern.length - 1]) {
-            document.getElementById("message").innerHTML = "Wrong Pattern. Game Over."
-            simonPattern = [];
-            userPattern = [];
-            return false;
-        }
-        if (userPattern.length === simonPattern.length) {
-            userPattern =[];
-            document.getElementById("message").innerHTML = "You won this round! Keep going!";
-            startRound();
-        }
+  userPattern.push(b)
+  if (userPattern[userPattern.length - 1] !== simonPattern[userPattern.length - 1]) {
+    message.innerHTML = "Wrong Pattern. Game Over."
+    simonPattern = [];
+    userPattern = [];
+    return false;
+  }
+  if (userPattern.length === simonPattern.length) {
+    userPattern = [];
+    message.innerHTML = "You won this round! Keep going!";
+    startRound();
+  }
 }
 
-for ( let i = 1; i <= Object.keys(simonButtons).length; i++) {
-    simonButtons[i].addEventListener('click', () => {
-        userInput(i)
-    });
+//Event Listeners
+for (let i = 1; i <= Object.keys(simonButtons).length; i++) {
+  simonButtons[i].addEventListener('click', () => {
+    userInput(i)
+  });
 }
 
 startButtons[1].addEventListener('click', () => {
-    document.getElementById("message").innerHTML = "Repeat the pattern";
-    startRound();
+  message.innerHTML = "Repeat the pattern";
+  startRound();
 });
 
 startButtons[2].addEventListener('click', () => {
-    simonPattern = [];
-    userPattern = [];
-    for (let i = 1; i < Object.keys(simonButtons).length; i++) {
-        simonButtons[i].style.opacity = 1;
-    }
-    document.getElementById("message").innerHTML = "Game reset.";
+  simonPattern = [];
+  userPattern = [];
+  for (let i = 1; i < Object.keys(simonButtons).length; i++) {
+    simonButtons[i].style.opacity = 1;
+  }
+  message.innerHTML = "Game reset.";
 })
 
